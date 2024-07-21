e1427650e16bbeba50058ac28f8a52a51528d794
painisreal <143738001+painisreal@users.noreply.github.com>
2024-07-21 13:52:25 +0530

    Add files via upload

---
+++ docs/scripts/mail.js
@@ -0,0 +1,25 @@
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbycIZPyakvukYR2oR8mbUuI57QfZn8X5c-Xtz5f1AiZNN7ZyFnMTvli58sjGkJ76GhcQw/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg');
+  const msgt = document.getElementById('msgt');
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+                msg.innerHTML = "Thanks for connecting...😁"
+                setTimeout(function(){
+                        msg.innerHTML = ""
+                },5000)
+                form.reset()
+              })
+              .catch(error => console.error('Error!', error.message))
+            })
+            
+            if (msg.innerHTML == "") {
+              function time(){
+                msgt.innerHTML = "Please wait..."
+                setTimeout(function(){
+                        msgt.innerHTML = ""
+                },4000)
+            }}
---
+++ docs/scripts/script.js
@@ -0,0 +1,8 @@
+// ! jQuery to insert other html pages
+$(function() {
+    $("#About").load("htmls/about.html");
+ });
+ 
+$(function() {
+    $("#Explore").load("htmls/explore.html");
+ });
