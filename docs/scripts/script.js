39759c656e323b887ce477d134c3fa7312ce237b
painisreal <ariknesh05@gmail.com>
2024-07-27 15:54:26 +0530

    feat!

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,8 +1,11 @@
 // ! jQuery to insert other html pages
 $(function() {
-    $("#About").load("htmls/about.html");
+    $("#About").load("htmls/members.html");
  });
  
 $(function() {
     $("#Explore").load("htmls/explore.html");
+ });
+$(function() {
+    $("#pop_up").load("htmls/popup.html");
  });
