f0a4d4b08220401b606bbe76eb15dd52519c3b56
painisreal <ariknesh05@gmail.com>
2024-07-30 00:13:21 +0530

    feat: added, !fix: bugs done

--- docs/scripts/script.js
+++ docs/scripts/script.js
@@ -1,5 +1,7 @@
 // ! jQuery to insert other html pages
 const popopen = document.getElementById('pop_up');
+const abt =document.getElementById('abt-container');
+const exp = document.getElementById('exp-container');
 $(function() {
     $("#About").load("htmls/members.html");
  });
@@ -11,3 +13,15 @@ $(function() {
  function open_popup(){
   popopen.style.display = "block";
  }
+function open_abt(){
+    abt.style.display = "block";
+}
+function close_abt(){
+    abt.style.display = "none";
+}
+function open_exp(){
+    exp.style.display = "block";
+}
+function close_exp(){
+    exp.style.display = "none";
+}
